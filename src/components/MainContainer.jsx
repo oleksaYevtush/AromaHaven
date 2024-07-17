bedffcfaea5d12bf8e1738dd022ccf7072b6c2ff
oleksaYevtush <olexaevtush@gmail.com>
2024-07-17 16:32:40 +0300

    fix: add default filter; fix category

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -8,17 +8,18 @@ import MenuContainer from './MenuContainer';
 import CartContainer from './CartContainer';
 
 const MainContainer = () => {
-  // eslint-disable-next-line no-unused-vars
   const [{ aromaItems, cartShow }, dispatch] = useStateValue();
   const [scrollValue, setScrollValue] = useState(0);
+  const [defaultCategory] = useState('candle');
 
   useEffect(() => {}, [scrollValue, cartShow]);
+
+  const filteredItems = aromaItems?.filter(n => n.category === defaultCategory);
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-auto">
       <HomeContainer />
-      <section
-        className="relative w-full p-6 px-10 py-6 mt-16 md:px-16 lg:px-20 before:w-full before:left-0 before:top-0 before:bg-mainTextGrey before:content before:absolute after:w-full after:left-0 after:bottom-0 after:bg-mainTextGrey after:content after:absolute">
+      <section className="relative w-full p-6 px-10 py-6 mt-16 md:px-16 lg:px-20 before:w-full before:left-0 before:top-0 before:bg-mainTextGrey before:content before:absolute after:w-full after:left-0 after:bottom-0 after:bg-mainTextGrey after:content after:absolute">
         <div className="flex flex-col items-center justify-between w-full h-full max-w-6xl px-4 mx-auto md:px-0">
           <p className="relative mb-12 text-2xl font-semibold transition-all duration-100 ease-in-out before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-br from-hoverColor to-[#cb57e085] text-mainColor text-center">
             Best Selling &{' '}
@@ -41,7 +42,7 @@ const MainContainer = () => {
           <RowContainer
             scrollValue={scrollValue}
             flag={true}
-            data={aromaItems?.filter(n => n.category === 'gift')}
+            data={filteredItems}
           />
         </div>
       </section>
